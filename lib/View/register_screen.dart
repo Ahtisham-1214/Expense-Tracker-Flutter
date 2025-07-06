14c17d86e643390a29667966ab784bd97dbf534a
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-07-06 20:12:43 +0500

    Register page, working successfully

---
+++ lib/View/register_screen.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import '../model/user_repository.dart';
+import '../model/user.dart';
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final UserRepository _userRepository = UserRepository();
+  final List<String> _roles = ['admin', 'user'];
+  String? _selectedRole = "user";
+
+
+  Future<void> _register() async {
+    if (_formKey.currentState!.validate()) {
+      final username = _usernameController.text;
+      final password = _passwordController.text;
+      final role = _selectedRole ?? 'user';
+
+      try {
+        final existingUser = await _userRepository.getUserByUsername(username);
+        if (existingUser != null) {
+          _showMessage("Username already exists", Color(0xFFC12222));
+        } else {
+          await _userRepository.insertUser(User(userName: username, password: password, role: role));
+          _showMessage("Registration Successful", Color(0xFF0E1B67));
+          // Optionally navigate back to login:
+          Future.delayed(const Duration(seconds: 2), () {
+            if (!mounted) return;
+            Navigator.pop(context);
+          });
+        }
+      } catch (e) {
+        _showMessage("An error occurred during registration", Color(0xFFC12222));
+      }
+    }
+  }
+
+  void _showMessage(String message, Color color) {
+    OverlayEntry? overlayEntry; // To keep track of the overlay entry
+
+    overlayEntry = OverlayEntry(
+      builder: (context) => SafeArea(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 80.0), // Distance from top
+            child: Material(
+              color: Colors.transparent,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                color: color,
+                elevation: 6,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0,
+                    vertical: 16.0,
+                  ),
+                  child: Text(
+                    message,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    // Add the OverlayEntry to the Overlay
+    Overlay.of(context).insert(overlayEntry);
+
+    // Remove the OverlayEntry after a duration
+    Future.delayed(const Duration(seconds: 1), () {
+      if (overlayEntry != null && overlayEntry!.mounted) {
+        overlayEntry?.remove();
+        overlayEntry = null; // Clear the reference
+      }
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.length < 3) {
+                    return 'Password must be at least 3 characters';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              DropdownButtonFormField<String>(
+                value: _selectedRole, // The currently selected value
+                decoration: const InputDecoration(
+                  labelText: 'Role',
+                  border: OutlineInputBorder(),
+                ),
+                hint: const Text('Select a role'), // Placeholder text
+                items: _roles.map((String role) {
+                  return DropdownMenuItem<String>(
+                    value: role,
+                    child: Text(role),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _selectedRole = newValue;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please select a user role';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _register,
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
