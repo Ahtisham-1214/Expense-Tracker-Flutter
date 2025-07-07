4b5b097beac7244d27d0116f87a943a60e880f7c
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-07-07 21:50:00 +0500

    Revert "Integrated firebase"
    This reverts commit 1f8e7eddbb4891e229fc9be40a8caea265157747.

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,9 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'Model/database_helper.dart';
 import 'View/login_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   await DatabaseHelper().database;
   runApp(const MyApp());
 }
